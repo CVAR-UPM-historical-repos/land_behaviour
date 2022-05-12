f5ea862a6d6e4183957d13094c2c54186e015757
Miguel <miferco97@gmail.com>
2022-05-12 15:01:07 +0200

    Sync Services client updated

--- land_behaviour/include/land_behaviour/land_behaviour.hpp
+++ land_behaviour/include/land_behaviour/land_behaviour.hpp
@@ -1,118 +1,126 @@
 #ifndef LAND_BEHAVIOUR_HPP
 #define LAND_BEHAVIOUR_HPP
 
+#include <as2_msgs/action/land.hpp>
 #include <chrono>
+#include <pluginlib/class_loader.hpp>
+#include <std_srvs/srv/set_bool.hpp>
 #include <thread>
 
 #include "as2_core/as2_basic_behaviour.hpp"
 #include "as2_core/names/actions.hpp"
-#include "as2_core/names/topics.hpp"
 #include "as2_core/names/services.hpp"
-
-#include <as2_msgs/action/land.hpp>
-#include <std_srvs/srv/set_bool.hpp>
-
-#include <pluginlib/class_loader.hpp>
+#include "as2_core/names/topics.hpp"
+#include "as2_core/synchronous_service_client.hpp"
 #include "land_plugin_base/land_base.hpp"
 
-#include "as2_core/synchronous_service_client.hpp"
+class LandBehaviour : public as2::BasicBehaviour<as2_msgs::action::Land> {
+  public:
+  using GoalHandleLand = rclcpp_action::ServerGoalHandle<as2_msgs::action::Land>;
+  using PSME = as2_msgs::msg::PlatformStateMachineEvent;
 
-class LandBehaviour : public as2::BasicBehaviour<as2_msgs::action::Land>
-{
-public:
-    using GoalHandleLand = rclcpp_action::ServerGoalHandle<as2_msgs::action::Land>;
-    using PSME = as2_msgs::msg::PlatformStateMachineEvent;
+  LandBehaviour()
+      : as2::BasicBehaviour<as2_msgs::action::Land>(as2_names::actions::behaviours::land) {
+    this->declare_parameter("default_land_plugin");
+    this->declare_parameter("default_land_speed");
 
-    LandBehaviour() : as2::BasicBehaviour<as2_msgs::action::Land>(as2_names::actions::behaviours::land)
-    {
-        this->declare_parameter("default_land_plugin");
-        this->declare_parameter("default_land_speed");
+    loader_ = std::make_shared<pluginlib::ClassLoader<land_base::LandBase>>("land_plugin_base",
+                                                                            "land_base::LandBase");
 
-        loader_ = std::make_shared<pluginlib::ClassLoader<land_base::LandBase>>("land_plugin_base", "land_base::LandBase");
-
-        try
-        {
-            land_speed_ = loader_->createSharedInstance(this->get_parameter("default_land_plugin").as_string());
-            land_speed_->initialize(this);
-            RCLCPP_INFO(this->get_logger(), "PLUGIN LOADED: %s", this->get_parameter("default_land_plugin").as_string().c_str());
-        }
-        catch (pluginlib::PluginlibException &ex)
-        {
-            RCLCPP_ERROR(this->get_logger(), "The plugin failed to load for some reason. Error: %s\n", ex.what());
-        }
-
-        state_machine_event_cli_ = this->create_client<as2_msgs::srv::SetPlatformStateMachineEvent>(
-            this->generate_global_name(as2_names::services::platform::set_platform_state_machine_event));
-        arming_cli_ = this->create_client<std_srvs::srv::SetBool>(as2_names::services::platform::set_arming_state);
-        if (state_machine_event_cli_->wait_for_service() && arming_cli_->wait_for_service())
-        {
-            RCLCPP_INFO(this->get_logger(), "Land Behaviour ready!");
-        }
-    };
-
-    rclcpp_action::GoalResponse onAccepted(const std::shared_ptr<const as2_msgs::action::Land::Goal> goal)
-    {
-        float land_speed = fabs(goal->land_speed);
-
-        as2_msgs::action::Land::Goal new_goal;
-        new_goal.land_speed = (land_speed != 0.0f) ? -fabs(land_speed) : -fabs(this->get_parameter("default_land_speed").as_double());
-        auto _goal = std::make_shared<const as2_msgs::action::Land::Goal>(new_goal);
-
-        RCLCPP_INFO(this->get_logger(), "LandBehaviour: Land with speed %f", _goal->land_speed);
-
-        return land_speed_->onAccepted(_goal);
+    try {
+      land_speed_ =
+          loader_->createSharedInstance(this->get_parameter("default_land_plugin").as_string());
+      land_speed_->initialize(this);
+      RCLCPP_INFO(this->get_logger(), "PLUGIN LOADED: %s",
+                  this->get_parameter("default_land_plugin").as_string().c_str());
+    } catch (pluginlib::PluginlibException &ex) {
+      RCLCPP_ERROR(this->get_logger(), "The plugin failed to load for some reason. Error: %s\n",
+                   ex.what());
     }
 
-    rclcpp_action::CancelResponse onCancel(const std::shared_ptr<GoalHandleLand> goal_handle)
-    {
-        return land_speed_->onCancel(goal_handle);
+    // state_machine_event_cli_ = this->create_client<as2_msgs::srv::SetPlatformStateMachineEvent>(
+    //     this->generate_global_name(as2_names::services::platform::set_platform_state_machine_event));
+    // arming_cli_ =
+    // this->create_client<std_srvs::srv::SetBool>(as2_names::services::platform::set_arming_state);
+    // if (state_machine_event_cli_->wait_for_service() && arming_cli_->wait_for_service())
+    // {
+    //     RCLCPP_INFO(this->get_logger(), "Land Behaviour ready!");
+    // }
+  };
+
+  rclcpp_action::GoalResponse onAccepted(
+      const std::shared_ptr<const as2_msgs::action::Land::Goal> goal) {
+    float land_speed = fabs(goal->land_speed);
+
+    as2_msgs::action::Land::Goal new_goal;
+    new_goal.land_speed = (land_speed != 0.0f)
+                              ? -fabs(land_speed)
+                              : -fabs(this->get_parameter("default_land_speed").as_double());
+    auto _goal = std::make_shared<const as2_msgs::action::Land::Goal>(new_goal);
+
+    RCLCPP_INFO(this->get_logger(), "LandBehaviour: Land with speed %f", _goal->land_speed);
+
+    if (!this->callStateMachineServer(PSME::LAND)) {
+      RCLCPP_ERROR(this->get_logger(), "Failed to call service state_machine_event");
     }
 
-    void onExecute(const std::shared_ptr<GoalHandleLand> goal_handle)
-    {
-        if (this->callStateMachineServer(PSME::LAND) != rclcpp::FutureReturnCode::SUCCESS)
-        {
-            RCLCPP_ERROR(this->get_logger(), "Failed to call service state_machine_event");
-        }
+    return land_speed_->onAccepted(_goal);
+  }
 
-        if (land_speed_->onExecute(goal_handle))
-        {
-            RCLCPP_INFO(this->get_logger(), "Land succeeded");
+  rclcpp_action::CancelResponse onCancel(const std::shared_ptr<GoalHandleLand> goal_handle) {
+    return land_speed_->onCancel(goal_handle);
+  }
 
-            if (this->callStateMachineServer(PSME::LANDED) != rclcpp::FutureReturnCode::SUCCESS)
-            {
-                RCLCPP_ERROR(this->get_logger(), "Failed to call service state_machine_event");
-            }
+  void onExecute(const std::shared_ptr<GoalHandleLand> goal_handle) {
+    if (land_speed_->onExecute(goal_handle)) {
+      RCLCPP_INFO(this->get_logger(), "Land succeeded");
 
-            auto req = std_srvs::srv::SetBool::Request().set__data(false);
-            auto future = arming_cli_->async_send_request(std::make_shared<std_srvs::srv::SetBool::Request>(req));
-            if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), future) != rclcpp::FutureReturnCode::SUCCESS)
-            {
-                RCLCPP_ERROR(this->get_logger(), "Unable to disarm");
-            }
-        }
-        else
-        {
-            RCLCPP_INFO(this->get_logger(), "Land canceled");
-        }
+      if (!this->callStateMachineServer(PSME::LANDED)) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to call service state_machine_event");
+      }
+
+      auto request = std_srvs::srv::SetBool::Request();
+      auto response = std_srvs::srv::SetBool::Response();
+      request.data = true;
+
+      auto disarm_cli = as2::SynchronousServiceClient<std_srvs::srv::SetBool>(
+          as2_names::services::platform::set_platform_state_machine_event);
+      bool out = disarm_cli.sendRequest(request, response);
+
+      if (out && response.success) {
+        return;
+      }
+
+      RCLCPP_ERROR(this->get_logger(), "Unable to disarm");
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Land canceled");
+      return;
     }
+  }
 
-private:
-    rclcpp::FutureReturnCode callStateMachineServer(const int8_t machine_event)
-    {
-        auto request = std::make_shared<as2_msgs::srv::SetPlatformStateMachineEvent::Request>();
-        request->event.event = machine_event;
+  private:
+  bool callStateMachineServer(const int8_t machine_event) {
+    auto request = as2_msgs::srv::SetPlatformStateMachineEvent::Request();
+    auto response = as2_msgs::srv::SetPlatformStateMachineEvent::Response();
 
-        auto future = state_machine_event_cli_->async_send_request(request);
-        return rclcpp::spin_until_future_complete(this->get_node_base_interface(), future);
+    request.event.event = machine_event;
+
+    auto set_mode_cli = as2::SynchronousServiceClient<as2_msgs::srv::SetPlatformStateMachineEvent>(
+        as2_names::services::platform::set_platform_state_machine_event);
+    bool out = set_mode_cli.sendRequest(request, response);
+
+    if (out && response.success) {
+      return true;
     }
+    return false;
+  }
 
-private:
-    std::shared_ptr<pluginlib::ClassLoader<land_base::LandBase>> loader_;
-    std::shared_ptr<land_base::LandBase> land_speed_;
+  private:
+  std::shared_ptr<pluginlib::ClassLoader<land_base::LandBase>> loader_;
+  std::shared_ptr<land_base::LandBase> land_speed_;
 
-    rclcpp::Client<as2_msgs::srv::SetPlatformStateMachineEvent>::SharedPtr state_machine_event_cli_;
-    rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr arming_cli_;
+  // rclcpp::Client<as2_msgs::srv::SetPlatformStateMachineEvent>::SharedPtr
+  // state_machine_event_cli_; rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr arming_cli_;
 };
 
-#endif // LAND_BEHAVIOUR_HPP
+#endif  // LAND_BEHAVIOUR_HPP
--- land_behaviour/src/land_behaviour_node.cpp
+++ land_behaviour/src/land_behaviour_node.cpp
@@ -1,17 +1,16 @@
 // "Copyright [year] <Copyright Owner>"
 
-#include "land_behaviour/land_behaviour.hpp"
 #include "as2_core/core_functions.hpp"
+#include "land_behaviour/land_behaviour.hpp"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<LandBehaviour>();
   node->preset_loop_frequency(30);
-  as2::spinLoop(node);  
-  
+  as2::spinLoop(node);
+
   rclcpp::shutdown();
   return 0;
 }
