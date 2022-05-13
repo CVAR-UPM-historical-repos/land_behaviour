7be2a41392b3f3f617c31c467c6668adc5d567fb
perezsaura-david <perezsaura.david@gmail.com>
2022-05-13 11:54:58 +0200

    v0.1.0

--- land_behaviour/include/land_behaviour/land_behaviour.hpp
+++ land_behaviour/include/land_behaviour/land_behaviour.hpp
@@ -1,3 +1,39 @@
+/*!*******************************************************************************************
+ *  \file       land_behaviour.hpp
+ *  \brief      land_behaviour header file
+ *  \authors    Miguel Fernández Cortizas
+ *              Pedro Arias Pérez
+ *              David Pérez Saura
+ *              Rafael Pérez Seguí
+ *
+ *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
+ *              All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
 #ifndef LAND_BEHAVIOUR_HPP
 #define LAND_BEHAVIOUR_HPP
 
@@ -34,7 +70,7 @@ public:
       land_plugin_ =
           loader_->createSharedInstance(this->get_parameter("default_land_plugin").as_string());
       land_plugin_->initialize(this);
-      RCLCPP_INFO(this->get_logger(), "PLUGIN LOADED: %s",
+      RCLCPP_INFO(this->get_logger(), "LAND PLUGIN LOADED: %s",
                   this->get_parameter("default_land_plugin").as_string().c_str());
     }
     catch (pluginlib::PluginlibException &ex)
@@ -98,7 +134,7 @@ public:
     }
     else
     {
-      RCLCPP_INFO(this->get_logger(), "Land canceled");
+      RCLCPP_WARN(this->get_logger(), "Land canceled");
       return;
     }
   }
